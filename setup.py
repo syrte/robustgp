59dc398dd8abd63f4dd89d9215f41cb9694e9583
syrte <styr.py@gmail.com>
2021-05-04 15:25:34 +0800

    version 2.1
    with updated document

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fp:
 
 setup(
     name='robustgp',
-    version='2.0',
+    version='2.1',
     description='Robust Gaussian process regression based on iterative trimming.',
     long_description=long_description,
     long_description_content_type='text/markdown',
